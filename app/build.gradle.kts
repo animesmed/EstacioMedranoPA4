8116a77ad1fda00189fa983e9f1d8d20e17fc394
Amilcar <bambino.amil.27@gmail.com>
2024-10-14 02:37:15 -0500

    Cambio de vista implementado con apartado para ubicacion gps en tiempo actual

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,13 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
